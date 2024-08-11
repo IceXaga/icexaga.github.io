79cf2a28af2082c5f05a1176074302829f8f114f
Xaga <103565166+IceXaga@users.noreply.github.com>
2024-08-11 17:31:41 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -48,8 +48,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'https://avatars.githubusercontent.com/u/103565166?v=4',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  name: 'IceXaga',
+  bio: 'qwq',
   links: [
     {
       name: 'Twitter',
